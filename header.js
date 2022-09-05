f7de1d075942d032ba53533c49f20bb9ae3650de
Julián Baliño <100325269+zLeaff@users.noreply.github.com>
2022-09-05 16:00:27 -0300

    Add files via upload
    Subheader dropdown

--- header.js
+++ header.js
@@ -2,21 +2,33 @@ const hamMenu = document.querySelector(".ham-menu");
 const navList = document.querySelector(".nav-list");
 const dropDownMenu = document.querySelector(".dropdown-menu");
 const subHeader = document.querySelector("#subheader_display_mobile")
+const subHeaderDropdown = document.querySelector("#subheader_dropdown")
+const dropdownContactList = document.querySelector(".dropdown-contact-list")
 
+////////////Subheader hide/////////////
 window.addEventListener('scroll', () => {
   let yScroll = window.scrollY;
 
   if (yScroll > 0) {
     subHeader.style.display = 'none'
   } else {
-    subHeader.style.display = 'flex'
+    subHeader.style.display = 'block'
   }
 });
 
+////////////Subheader dropdown/////////////
+subHeaderDropdown.addEventListener("click", () => {
+  dropdownContactList.classList.toggle("active")
+});
+
+
+////////////Navbar dropdown menu/////////////
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     dropDownMenu.classList.toggle("active");
   });
+  
+
   
 document.querySelectorAll(".nav-links").forEach((link) =>
     link.addEventListener("click", () => {
